99a39bd9b8ee3feead0efdd45be2842a5fa7b550
纪宇伟 <jiyuwei@jiyuweideMacBook-Pro.local>
2017-06-21 23:11:43 +0800

    fin

--- SpinDiscDemo/AppDelegate.m
+++ SpinDiscDemo/AppDelegate.m
@@ -41,13 +41,13 @@
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    [[NSNotificationCenter defaultCenter] postNotificationName:@"CheckPlayStatus" object:nil];
+    
 }
 
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"CheckPlayStatus" object:nil];
 }
 
 
